6123f90a7cbf1c412e0f67062c39a82eb33c19e7
Mathias Törnblom <mathias@tornbloms.net>
2024-07-31 22:29:20 +0200

    Linted

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
 import Homey from 'homey';
+
 class BlueAirApp extends Homey.App {
-    /**
-     * onInit is called when the app is initialized.
-     */
-    async onInit(): Promise<void> {
-        this.log('BlueAir has been initialized');
-    }
+  /**
+   * onInit is called when the app is initialized.
+   */
+  async onInit(): Promise<void> {
+    this.log('BlueAir has been initialized');
+  }
 }
 
 module.exports = BlueAirApp;
