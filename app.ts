941f5d61f91d88147c3ff23bef6988dc8d81b8d7
Mathias Törnblom <mathias@tornbloms.net>
2024-08-05 00:49:31 +0200

    Added Homey-Log

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 import Homey from 'homey';
+const { Log } = require('homey-log');
 
 class BlueAirApp extends Homey.App {
+  homeyLog: any;
+
   /**
    * onInit is called when the app is initialized.
    */
   async onInit(): Promise<void> {
+    this.homeyLog = new Log({ homey: this.homey });
     this.log('BlueAir has been initialized');
   }
 }
