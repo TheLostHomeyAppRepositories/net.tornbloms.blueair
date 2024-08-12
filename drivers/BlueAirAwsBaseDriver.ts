f9e2634f00463e3dc47ee001a472a7c6ab6f57a9
Mathias Törnblom <mathias@tornbloms.net>
2024-08-12 13:01:54 +0200

    Moved common logic for the driver

---
+++ drivers/BlueAirAwsBaseDriver.ts
@@ -0,0 +1,112 @@
+import { Driver } from 'homey';
+import { BlueAirAwsClient } from 'blueairaws-client';
+import { Region } from 'blueairaws-client/dist/Consts';
+
+export default abstract class BlueAirAwsBaseDriver extends Driver {
+  protected abstract deviceModelFilter: string;
+
+  /**
+   * onInit is called when the driver is initialized.
+   */
+  async onInit(): Promise<void> {
+    this.log(`${this.constructor.name} has been initialized`);
+  }
+
+  /**
+   * Handles the pairing process.
+   * @param session - The pairing session.
+   */
+  async onPair(session: any): Promise<void> {
+    let username = '';
+    let password = '';
+
+    session.setHandler(
+      'login',
+      async (data: { username: string; password: string }) => {
+        username = data.username;
+        password = data.password;
+
+        try {
+          const client = new BlueAirAwsClient(username, password);
+          return await client.initialize();
+        } catch (e) {
+          this.log(e);
+          return false;
+        }
+      },
+    );
+
+    session.setHandler('list_devices', async () => {
+      try {
+        const client = new BlueAirAwsClient(username, password);
+
+        if (await client.initialize()) {
+          const devicesList = await client.getDevices();
+
+          if (!Array.isArray(devicesList)) {
+            throw new Error('devicesList is not an array');
+          }
+
+          const compatibleDevices = await this.filterCompatibleDevices(
+            devicesList,
+            client,
+            username,
+            password,
+          );
+
+          return compatibleDevices;
+        }
+        return null;
+      } catch (error) {
+        this.log('Error listing devices:', error);
+        return { error: 'Failed to list devices' };
+      }
+    });
+  }
+
+  /**
+   * Filters the list of devices based on the specific model.
+   * @param devicesList - The list of devices from the API.
+   * @param client - The BlueAir API client.
+   * @param username - The user's username.
+   * @param password - The user's password.
+   * @returns A list of compatible devices.
+   */
+  protected async filterCompatibleDevices(
+    devicesList: any[],
+    client: BlueAirAwsClient,
+    username: string,
+    password: string,
+  ) {
+    const compatibleDevices = [];
+    const accountuuid = devicesList[0].name;
+
+    for (const device of devicesList) {
+      const deviceInfoArray = await client.getDeviceStatus(accountuuid, [
+        device.uuid,
+      ]);
+
+      for (const deviceInfo of deviceInfoArray) {
+        if (deviceInfo.model.toLowerCase().includes(this.deviceModelFilter)) {
+          compatibleDevices.push({
+            name: deviceInfo.name,
+            data: {
+              accountuuid,
+              uuid: deviceInfo.id,
+              mac: device.mac,
+            },
+            store: {
+              name: deviceInfo.name,
+            },
+            settings: {
+              username,
+              password,
+              region: Region.EU,
+            },
+          });
+        }
+      }
+    }
+    return compatibleDevices;
+  }
+}
--- drivers/dustmagnet/driver.ts
+++ drivers/dustmagnet/driver.ts
@@ -1,103 +1,15 @@
-import { Driver } from 'homey';
-import { BlueAirAwsClient } from 'blueairaws-client';
-import { Region } from 'blueairaws-client/dist/Consts';
+import BlueAirAwsBaseDriver from '../BlueAirAwsBaseDriver.ts';
 
-class BlueAirDustMagnetDriver extends Driver {
+class BlueAirDustMagnetDriver extends BlueAirAwsBaseDriver {
+  protected deviceModelFilter = 'dustmagnet';
+
   /**
    * onInit is called when the driver is initialized.
    */
-  async onInit() {
+  async onInit(): Promise<void> {
     this.log('BlueAir DustMagnet Driver has been initialized');
-  }
-
-  async onPair(session: any): Promise<void> {
-    let username = '';
-    let password = '';
-
-    // Pairing sequences
-    session.setHandler(
-      'login',
-      async (data: { username: string; password: string }) => {
-        username = data.username;
-        password = data.password;
-
-        try {
-          const client = new BlueAirAwsClient(username, password);
-          return await client.initialize(); // Use await here to correctly update the status
-        } catch (e) {
-          this.log(e);
-          return false;
-        }
-      },
-    );
-
-    // Define a session handler for listing devices
-    session.setHandler('list_devices', async () => {
-      try {
-        // Create a new API client instance with the provided username and password
-        const client = new BlueAirAwsClient(username, password);
-
-        // Initialize the API client
-        if (await client.initialize()) {
-          // Fetch the list of devices from the API
-          const devicesList = await client.getDevices();
-
-          // Validate that the returned devicesList is indeed an array
-          if (!Array.isArray(devicesList)) {
-            throw new Error('devicesList is not an array');
-          }
-          this.log(devicesList);
-
-          // Initialize an empty array to store devices that meet the compatibility criteria
-          const compatibleDevices = [];
-
-          // Using the first device name as accountuuid
-          const accountuuid = devicesList[0].name;
-
-          // Iterate over each device in the devicesList
-          for (const device of devicesList) {
-            // Fetch detailed information for the current device using its UUID
-            const deviceInfoArray = await client.getDeviceStatus(accountuuid, [
-              device.uuid,
-            ]);
-
-            // Iterate over each deviceInfo in the deviceInfoArray
-            for (const deviceInfo of deviceInfoArray) {
-              // Check if the device's compatibility matches the desired value
-              if (deviceInfo.model.toLowerCase().includes('DustMagnet')) {
-                // If the device is compatible, add its details to the compatibleDevices array
-                compatibleDevices.push({
-                  name: deviceInfo.name,
-                  data: {
-                    accountuuid: devicesList[0].name,
-                    uuid: deviceInfo.id,
-                    mac: device.mac,
-                  },
-                  store: {
-                    name: deviceInfo.name,
-                  },
-                  settings: {
-                    username,
-                    password,
-                    region: Region.EU,
-                  },
-                });
-              }
-            }
-          }
-
-          // Return the list of compatible devices
-          return compatibleDevices;
-        }
-        return null;
-      } catch (error) {
-        // Log any errors that occur during the process
-        this.log('Error listing devices:', error);
-        // Return an appropriate error response
-        return { error: 'Failed to list devices' };
-      }
-    });
+    await super.onInit(); // Call the base class's onInit method
   }
 }
 
-module.exports = BlueAirDustMagnetDriver;
+export default BlueAirDustMagnetDriver;
